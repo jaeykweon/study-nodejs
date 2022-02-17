690089efbfa0b0ab12d85482664181dc88a739c9
jay <jaeykweon@naver.com>
2022-02-17 23:52:21 +0900

    add: promise all

---
+++ promise-all.js
@@ -0,0 +1,24 @@
+const { log, timer, timerEnd } = require('./utils/log');
+
+function makePromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, 200); // calling youtube api needs almost 200ms
+    }).then(function() {
+        // do something
+    });
+}
+
+const promises = [];
+const count = 1000000;  // change this
+for(i=1;i<count;i++){
+    const promise = makePromise();
+    promises.push(promise);
+}
+
+log("number of promises: ", count);
+timer("check promise all");
+Promise.all(promises)
+    .then(() => {
+        log("done");
+        timerEnd("check promise all");
+    })
---
+++ utils/log.js
@@ -0,0 +1,9 @@
+const log = console.log;
+const timer = console.time;
+const timerEnd = console.timeEnd;
+
+module.exports = {
+	log,
+	timer,
+	timerEnd
+}
